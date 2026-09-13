a9cdc853c02cc81ce540bf30f6b2b185276cd54e
aasthaa8 <aasthasingh.official8@gmail.com>
2026-09-13 12:25:40 +0530

    Max Subarray With 1 Deletion Time: O(n), Space: O(1)

---
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maximumSum(vector<int>& arr) {
+        if(arr.size()==1)
+        {
+            return arr[0];
+        }
+        int no_delete=arr[0];
+        int one_delete=INT_MIN;
+        int res=arr[0];
+        for(int i=1;i<arr.size();i++)
+        {
+            int prev_no_delete=no_delete;
+            int prev_one_delete=one_delete;
+            no_delete=max(no_delete+arr[i], arr[i]);
+            int v;
+            if(prev_one_delete==INT_MIN)
+            {
+                v=arr[i];
+            }
+            else
+            {
+                v=one_delete+arr[i];
+            }
+            one_delete=max(v,prev_no_delete);
+            res=max(res,max(no_delete,one_delete));
+        }
+        return res;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
