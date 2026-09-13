2118588c2cf055111fed4baea393c78737160832
aasthaa8 <aasthasingh.official8@gmail.com>
2026-09-13 12:28:24 +0530

    Max Subarray with 1 deletion- simplified Kadane's Algorithm Time: O(n), Space: O(1)

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
@@ -6,23 +6,14 @@ public:
             return arr[0];
         }
         int no_delete=arr[0];
-        int one_delete=INT_MIN;
+        int one_delete=0;
         int res=arr[0];
         for(int i=1;i<arr.size();i++)
         {
             int prev_no_delete=no_delete;
             int prev_one_delete=one_delete;
             no_delete=max(no_delete+arr[i], arr[i]);
-            int v;
-            if(prev_one_delete==INT_MIN)
-            {
-                v=arr[i];
-            }
-            else
-            {
-                v=one_delete+arr[i];
-            }
-            one_delete=max(v,prev_no_delete);
+            one_delete=max(one_delete+arr[i],prev_no_delete);
             res=max(res,max(no_delete,one_delete));
         }
         return res;
