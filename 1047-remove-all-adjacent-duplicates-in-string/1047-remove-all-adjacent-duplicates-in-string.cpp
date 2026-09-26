b23b7a561260766fd78eedbf6d8d3f93a1ea3198
aasthaa8 <aasthasingh.official8@gmail.com>
2026-09-26 14:49:37 +0530

    Remove All Adjacent Duplicates-Stack Time: O(n), Space: O(n)

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string res;
+        stack<char> st;
+        for(int i=0;i<s.size();i++)
+        {
+            if(st.empty())
+            {
+                st.push(s[i]);
+                continue;
+            }
+
+            if(st.top()==s[i])
+            {
+                st.pop();
+                continue;
+            }
+            st.push(s[i]);
+        }
+
+        while(!st.empty())
+        {
+            res.push_back(st.top());
+            st.pop();
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
